57431db4a5c338c7a067d651bcec0913834f4a04
Mathus <mathus9322@gmail.com>
2025-04-15 00:18:42 +0000

    fin du tp

--- index.php
+++ index.php
@@ -1,22 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./style.css">
     <title>Generateur de BART</title>
 </head>
+
 <body>
     <div class="tableau-div">
-        
 
-    <?php
-        for($i=0;$i<100;$i++){
+
+        <?php
+
+        if (isset($_GET['nb_lignes'])) {
+            $nb_ligne = $_GET['nb_lignes'];
+        }
+        else{
+            $nb_ligne = 10;
+        }
+
+        for ($i = 0; $i < $nb_ligne; $i++) {
             echo "<p>Je ne recopierai pas l'exemple de valentin</p>";
         }
-    ?>
+        ?>
 
     </div>
-    
+
 </body>
+
 </html>
